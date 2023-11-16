5af42e10a6aefc4f933ec1ad85db80ba0cf9cb34
mohammed <souarimohammed@gmail.com>
2023-11-16 01:12:58 +0100

    add create getall getall with condition

--- server.js
+++ server.js
@@ -1,15 +1,62 @@
 const express = require('express');
+const Pokemon = require('./models/pokemon')
 require('./config/connect')
 
 const app = express();
+app.use(express.json())
 
-app.post( '/add' , ()=>{
-    console.log('add fct');
+app.post( '/add' , (req,res)=>{
+   
+    const data = req.body;
+    pkm = new Pokemon(data);
+    pkm.save()
+    .then(
+        (savedObject)=>{
+            res.send(savedObject)
+        }
+    )
+    .catch(
+        (e)=>{
+            res.send(e)
+        }
+    )
 
 })
-app.get('/getAll',()=>{
-    console.log('get all fct');
+app.post( '/create' , async (req,res)=>{
+    try {
+        const data = req.body;
+    pkm = new Pokemon(data);
+    savedObject = await pkm.save()
+    res.send(savedObject)
+        
+    } catch (error) {
+        res.send(error)
+    }
+
 })
+app.get('/getAll', (req, res) => {
+    Pokemon.find()
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+  
+  app.get('/getAllPokemon', async (req, res) => {
+    try {
+        data = await Pokemon.find({name : "bulbazor"});
+         res.send(data);
+        
+    } catch (error) {
+        res.send(error)
+         
+    }
+  })
+
+
+
 
 app.put('/put',()=>{
     console.log('put fct');
